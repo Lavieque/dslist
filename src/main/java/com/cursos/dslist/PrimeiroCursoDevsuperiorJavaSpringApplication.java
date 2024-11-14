47dc26b48bf810448bdda60d49bfbecaaa9aae4c
Lavieque <virgiliojoselavieque@gmail.com>
2024-11-14 22:02:36 +0000

    Project created

---
+++ src/main/java/com/cursos/dslist/PrimeiroCursoDevsuperiorJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cursos.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroCursoDevsuperiorJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroCursoDevsuperiorJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursos/dslist/PrimeiroCursoDevsuperiorJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursos.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroCursoDevsuperiorJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
